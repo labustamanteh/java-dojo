eea3c44e8f0583aaa70c7ff47120ea990969bc04
labustamante_meli <lauravanesa.bustamante@mercadolibre.com.co>
2025-03-31 14:08:00 -0500

    add exceptions exercise

--- tre-poo/src/Main.java
+++ tre-poo/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // clases y objetos
         Persona laura = new Persona();
         Persona lucas = new Persona("lucas", 23, "12456321");
         Persona carolina = new Persona("carolina", 24, "12456321", 60, 1.62);
@@ -13,6 +14,19 @@ public class Main {
             System.out.println("Es mayor de edad");
         } else {
             System.out.println("Es menor de edad");
+        }
+
+        // excepciones
+        Integer a = 0;
+        Integer b = 300;
+
+        try {
+            Integer cociente = b / a;
+        } catch (ArithmeticException e) {
+            // System.out.println("Se ha producido un error");
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        } finally {
+            System.out.println("Programa finalizado");
         }
     }
 }
